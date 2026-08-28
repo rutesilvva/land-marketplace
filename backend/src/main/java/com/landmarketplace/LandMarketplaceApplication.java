e3fee98e769b80cbc3c0e0c8439db46235514d54
rutesilvva <Rute.silva@ccc.ufcg.edu.br>
2026-08-28 17:27:00 -0300

    build: bootstrap Spring Boot API

---
+++ backend/src/main/java/com/landmarketplace/LandMarketplaceApplication.java
@@ -0,0 +1,12 @@
+package com.landmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandMarketplaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LandMarketplaceApplication.class, args);
+    }
+}
+
